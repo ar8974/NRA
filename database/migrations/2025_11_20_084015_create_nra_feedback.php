9b3472f4d94e794cf344c55ae6166401c1785314
akash-onepercent <onepercent.akash@gmail.com>
2025-12-10 10:21:29 -0500

    Partitions

--- database/migrations/2025_11_20_084015_create_nra_feedback.php
+++ database/migrations/2025_11_20_084015_create_nra_feedback.php
@@ -3,12 +3,11 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('nra_feedback', function (Blueprint $table) {
@@ -18,15 +17,26 @@ return new class extends Migration
             $table->integer('View_ID')->notNullable();
             $table->integer('Series_ID')->notNullable();
 
-            $table->primary(['View_ID','Series_ID']);
+            $table->primary(['View_ID', 'Series_ID']);
+
             $table->foreign('View_ID')->references('View_ID')->on('nra_viewer');
             $table->foreign('Series_ID')->references('Series_ID')->on('nra_webseries');
+
+            $table->index('Rating');
+            $table->index('Feedback_date');
         });
+
+        DB::statement("
+            ALTER TABLE nra_feedback
+            PARTITION BY RANGE (YEAR(Feedback_date)) (
+                PARTITION p2022 VALUES LESS THAN (2023),
+                PARTITION p2023 VALUES LESS THAN (2024),
+                PARTITION p2024 VALUES LESS THAN (2025),
+                PARTITION pmax VALUES LESS THAN MAXVALUE
+            )
+        ");
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('nra_feedback');
